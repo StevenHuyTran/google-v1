3d044dc0a5ac861874ed46f9aa6063b784d0f074
Steven <Stevenhuytran03@gmail.com>
2023-03-14 14:16:40 -0700

    Added pagination component

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Parser from "html-react-parser";
+import PaginationButtons from "./PaginationButtons";
 
 export default function SearchResults({ results }) {
   return (
@@ -19,6 +20,7 @@ export default function SearchResults({ results }) {
           <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
         </div>
       ))}
+      <PaginationButtons/>
     </div>
   );
 }
